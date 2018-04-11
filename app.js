ccfc1c38b9ca4136a6466a4a32e561246aff066c
travo100 <travo100@gmail.com>
2018-04-10 17:15:20 -0700

    Added base line for our weather and time app

---
+++ app.js
@@ -0,0 +1,35 @@
+var inquirer = require('inquirer');
+var weather = require('weather-js');
+var moment = require('moment');
+
+inquirer.prompt([
+    {
+        "type": "list",
+        "message": "Would you like to check the time or the weather?",
+        "name": "weatherOrTime",
+        "choices": ["time", "weather"]
+    }
+]).then(function (answers) {
+    // if our user picks the weather show the weather data
+    if (answers.weatherOrTime === "weather") {
+        inquirer.prompt([
+            {
+                "type": "input",
+                "message": "What city do you want to look for the weather in?",
+                "name": "city"
+            }
+        ]).then(function (answers) {
+            // console.log("show the weather")
+            weather.find({ search: answers.city, degreeType: 'F' }, function (err, result) {
+                if (err) console.log(err);
+                console.log("The weather in " + result[0].location.name + " is currently " + result[0].current.skytext + " and " + result[0].current.temperature + " degrees.");
+            });
+        });
+
+    } else {
+        // if they pick the time show the current time to the user
+        console.log(moment().format("h:mm:ss a"));
+    }
+}).catch(function (error) {
+    console.log(error);
+});
---
+++ results.js
@@ -0,0 +1,80 @@
+var results = {
+  location:
+    {
+      name: 'San Diego, CA',
+      zipcode: undefined,
+      lat: '32.716',
+      long: '-117.162',
+      timezone: '-7',
+      alert: '',
+      degreetype: 'F',
+      imagerelativeurl: 'http://blob.weather.microsoft.com/static/weather4/en-us/'
+    },
+  current:
+    {
+      temperature: '76',
+      skycode: '32',
+      skytext: 'Sunny',
+      date: '2018-04-10',
+      observationtime: '16:40:00',
+      observationpoint: 'San Diego, CA',
+      feelslike: '76',
+      humidity: '41',
+      winddisplay: '9 mph West',
+      day: 'Tuesday',
+      shortday: 'Tue',
+      windspeed: '9 mph',
+      imageUrl: 'http://blob.weather.microsoft.com/static/weather4/en-us/law/32.gif'
+    },
+  forecast:
+    [{
+      low: '56',
+      high: '75',
+      skycodeday: '31',
+      skytextday: 'Clear',
+      date: '2018-04-09',
+      day: 'Monday',
+      shortday: 'Mon',
+      precip: ''
+    },
+    {
+      low: '61',
+      high: '80',
+      skycodeday: '34',
+      skytextday: 'Mostly Sunny',
+      date: '2018-04-10',
+      day: 'Tuesday',
+      shortday: 'Tue',
+      precip: '0'
+    },
+    {
+      low: '58',
+      high: '69',
+      skycodeday: '32',
+      skytextday: 'Sunny',
+      date: '2018-04-11',
+      day: 'Wednesday',
+      shortday: 'Wed',
+      precip: '0'
+    },
+    {
+      low: '56',
+      high: '67',
+      skycodeday: '32',
+      skytextday: 'Sunny',
+      date: '2018-04-12',
+      day: 'Thursday',
+      shortday: 'Thu',
+      precip: '50'
+    },
+    {
+      low: '58',
+      high: '73',
+      skycodeday: '32',
+      skytextday: 'Sunny',
+      date: '2018-04-13',
+      day: 'Friday',
+      shortday: 'Fri',
+      precip: '0'
+    }]
+}
